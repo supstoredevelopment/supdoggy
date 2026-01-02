23c8a8f93a22e934f78d5e3af2cb7aeee18d6fbc
hugo <supstoredevelopment@gmail.com>
2026-01-02 20:21:20 +0100

    commit message

---
+++ src/js/contact.js
@@ -0,0 +1,15 @@
+function sendMail() {
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        title: document.getElementById("subject").value,   
+        message: document.getElementById("message").value,
+    };
+
+    return emailjs.send(
+        "service_0vbieb1",
+        "template_jcj4ako",
+        params
+    );
+}
